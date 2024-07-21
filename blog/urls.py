0b6d3be608e8c4db6bbc578e77b837e5152fc6ba
aedwardz <antonile@uci.edu>
2024-07-20 18:16:32 -0700

    Created Skeleton views and URL routes

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.homePage)
+    path("", views.home_page),
+    path("posts/", views.posts_page),
+    path("posts/<str:post>, views.single_post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def homePage(request):
+def home_page(request):
     return render(request, "blog/blog.html")
+
+def posts_page(request):
+    pass
+
+def single_post(request, post: str):
+    pass
